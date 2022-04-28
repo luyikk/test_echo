a76f7e51b1f724f19fb312d8e53b9ce15d0e66e6
luyikk <luyikk@126.com>
2022-04-28 17:50:57 +0800

    add

--- echo-server/src/main.rs
+++ echo-server/src/main.rs
@@ -1,34 +1,71 @@
-use async_std::{io, task};
-use async_std::net::TcpListener;
-use async_std::prelude::*;
-use anyhow::Result;
+// //use async_std::{io, task};
+// //use async_std::net::TcpListener;
+// //use async_std::prelude::*;
+// use anyhow::Result;
+//
+// #[tokio::main]
+// async fn main()->Result<()> {
+//     #[cfg(target_os = "linux")]
+//     {
+//         println!("set limit max open file:{}", 200000);
+//         rlimit::Resource::NOFILE.set(200000, 200000)?;
+//     }
+//
+//     // let listener = TcpListener::bind("127.0.0.1:2000").await?;
+//     // let mut incoming = listener.incoming();
+//     //
+//     // while let Some(stream) = incoming.next().await {
+//     //     let stream = stream?;
+//     //     tokio::spawn(async move{
+//     //         let (reader, writer) = &mut (&stream, &stream);
+//     //
+//     //         loop {
+//     //             let mut b = [0u8];
+//     //             if reader.read(&mut b).await? == 0 {
+//     //                 break;
+//     //             }
+//     //             writer.write(&b).await?;
+//     //         }
+//     //         //let _=io::copy(reader, writer).await;
+//     //     });
+//     // }
+//
+//
+//
+//     Ok(())
+// }
 
-#[async_std::main]
-async fn main()->Result<()> {
-    #[cfg(target_os = "linux")]
-    {
-        println!("set limit max open file:{}", 200000);
-        rlimit::Resource::NOFILE.set(200000, 200000)?;
-    }
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-    let listener = TcpListener::bind("127.0.0.1:2000").await?;
-    let mut incoming = listener.incoming();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    while let Some(stream) = incoming.next().await {
-        let stream = stream?;
-        task::spawn(async move{
-            let (reader, writer) = &mut (&stream, &stream);
+    loop {
+        let (mut socket, _) = listener.accept().await?;
 
+        tokio::spawn(async move {
+            let mut buf = [0; 1];
+
+            // In a loop, read data from the socket and write the data back.
             loop {
-                let mut b = [0u8];
-                if reader.read(&mut b).await? == 0 {
-                    break;
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
                 }
-                writer.write(&b).await?;
             }
-            //let _=io::copy(reader, writer).await;
         });
     }
-
-    Ok(())
 }
