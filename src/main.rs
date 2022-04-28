cad4951ee94dd12696d71bd9f30ad9d608b7ac42
luyikk <luyikk@126.com>
2022-04-28 15:07:05 +0800

    add linux limit check

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,31 @@ use anyhow::Result;
 use structopt::StructOpt;
 
 #[async_std::main]
-async fn main()->Result<()> {
-    let opt:Opt = Opt::from_args();
+async fn main() -> Result<()> {
+    let opt: Opt = Opt::from_args();
 
-
-    for connect_id in 0..opt.connects {
-
-
-
+    #[cfg(target_os = "linux")]
+    {
+        let (soft, hard) = rlimit::Resource::NOFILE.get()?;
+        if soft < opt.connects {
+            println!("set limit max open file:{}", opt.connects * 2);
+            rlimit::Resource::NOFILE.set(opt.connects * 2, opt.connects * 2)?;
+        }
     }
+
+
+
+    for connect_id in 0..opt.connects {}
     Ok(())
 }
 
 #[derive(StructOpt)]
 #[structopt(name = "test echo tool")]
-struct Opt{
+struct Opt {
     /// addrs: 127.0.0.1:20000
     #[structopt(short, long)]
-    addrs:String,
+    addrs: String,
     /// connect number
     #[structopt(short, long)]
-    connects:u64
+    connects: u64,
 }
